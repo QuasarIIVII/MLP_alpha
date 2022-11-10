647d32e8e70587011568648d9ad3f3c7066987a3
QuasarIIVII <zest8011@gmail.com>
2022-11-10 10:51:42 +0000

    221110B
    MLP Complete A

--- a.cpp
+++ a.cpp
@@ -7,26 +7,27 @@ long double ELU(long double x){return x<0 ? expl(x)-1 : x;}
 long double dELU(long double x){return x<0 ? expl(x) : 1;}
 long double _tanh(long double x){return (tanh(x)+1)/2;}
 int main(){
-	{
-		cout<<"H"<<endl;
-		SLPa::SLPa<int, long double> a;
-		a._p();
-		a=SLPa::SLPa<int, long double>(12);
-		a._p();
-		a._s(3,0.7);
-		SLPa::SLPa<int, long double> b=a;
-		b._p();
-	}
-	{
-		SLPa::SLPa<int, long double> a(2);
-		a._s(0,1.0);
-		a._s(1,1.0);
-		a._p();
-		long double l0[]={2,-2};
-		long double l1[4][3]={{0,0,-1},{0,1,1},{1,0,1},{1,1,1}};
-		for(int64_t i=1<<10;i;i--)for(int j=3;j+1;j--)a.train(l1[j],0.01,dCost);
-		cout<<a.calc(l1[0])<<' '<<a.calc(l1[1])<<' '<<a.calc(l1[2])<<' '<<a.calc(l1[3])<<endl;
-	}
+	cout<<"Powered by Rúhûlù"<<endl;
+//	{
+//		cout<<"H"<<endl;
+//		SLPa::SLPa<int, long double> a;
+//		a._p();
+//		a=SLPa::SLPa<int, long double>(12);
+//		a._p();
+//		a._s(3,0.7);
+//		SLPa::SLPa<int, long double> b=a;
+//		b._p();
+//	}
+//	{
+//		SLPa::SLPa<int, long double> a(2);
+//		a._s(0,1.0);
+//		a._s(1,1.0);
+//		a._p();
+//		long double l0[]={2,-2};
+//		long double l1[4][3]={{0,0,-1},{0,1,1},{1,0,1},{1,1,1}};
+//		for(int64_t i=1<<10;i;i--)for(int j=3;j+1;j--)a.train(l1[j],0.01,dCost);
+//		cout<<a.calc(l1[0])<<' '<<a.calc(l1[1])<<' '<<a.calc(l1[2])<<' '<<a.calc(l1[3])<<endl;
+//	}
 	cout<<"H1"<<endl;
 //	{
 //		int l0[]={3,4,5,2}, l1[]={2,3,4,7,3,1,7};
@@ -74,7 +75,7 @@ int main(){
 
 		a._p();
 
-		for(int t=1<<10;t;t--)for(int i=0;i<4;i++)a.train(l2[i], 0.001, dCost);
+		for(int t=1<<13;t;t--)for(int i=0;i<4;i++)a.train(l2[i], 0.001, dCost);
 
 		a._p();
 
@@ -83,4 +84,5 @@ int main(){
 			for(int j=0;j<16;j++)cout<<r[j]<<' ';cout<<endl;
 		}
 	}
+	cout<<"Powered by Rúhûlù"<<endl;
 }
