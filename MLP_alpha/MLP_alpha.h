a47082275e1ab004f7c6c7d3f39be4d6ee472b12
QuasarIIVII <zest8011@gmail.com>
2022-11-10 02:18:31 +0000

    221110A

--- MLP_alpha/MLP_alpha.h
+++ MLP_alpha/MLP_alpha.h
@@ -4,6 +4,18 @@
 #include<iostream>
 #include<iomanip>
 #include<math.h>
+#include<time.h>
+#include<sys/time.h>
+#include<stdlib.h>
+long double randl(long double min, long double max){
+	using namespace std;
+	cout<<max-min<<endl;
+	static unsigned int a;
+	struct timeval tv;
+	gettimeofday(&tv,NULL);
+	srand((long long)tv.tv_sec*1000000+tv.tv_usec+a&0xFFFFFFFF);
+	return (long double)((long long)(a=rand())*RAND_MAX*RAND_MAX+rand()*RAND_MAX+rand())*(max-min)/RAND_MAX/RAND_MAX+min;
+}
 long double linear(long double x){return x;}
 long double _dtanh(long double x){return 2/(coshl(x*2)+1);}
 namespace MLPa{
@@ -21,8 +33,8 @@ namespace SLPa{
 		}num;
 		private:_rFloat*weight=0;
 		private:typedef struct _sS0{
-			_rFloat (*activatoin)(_rFloat);
-			_rFloat (*dactivatoin)(_rFloat);
+			_rFloat (*activation)(_rFloat);
+			_rFloat (*dactivation)(_rFloat);
 		}_sS0;
 		private:_sS0 funcs;
 		
@@ -38,8 +50,9 @@ namespace SLPa{
 			;//std::cout<<"CA"<<std::endl;
 			this->num.weight=numOfWeights;
 			this->weight=new _rFloat[numOfWeights+1];
-			this->funcs.activatoin=&tanhl; // ========Need to handle NullPtrException========
-			this->funcs.dactivatoin=&_dtanh;
+			for(_rInt i=numOfWeights;i+1;i--)weight[i]=randl(0.0L,10.0L), cout<<"w : "<<weight[i]<<endl;
+			this->funcs.activation=&tanhl; // ========Need to handle NullPtrException========
+			this->funcs.dactivation=&_dtanh;
 			//weight[num.weight] -> bias
 		}
 		//Destructor
@@ -68,11 +81,21 @@ namespace SLPa{
 		
 		//Member functions
 		public:_rFloat calc(_rFloat*data){
+//			cout<<data[0]<<endl;
+			cout<<"SLP Calc : "<<num.weight<<endl;
 			_rFloat s=0;
 			_rInt i;
 			for(i=0; i<num.weight; i++)s+=data[i]*weight[i];
 			s+=weight[i];
-			return funcs.activatoin(s);
+			return funcs.activation(s);
+		}
+		private:_rFloat calcDeAct(_rFloat*data){
+//			cout<<data[0]<<endl;
+//			cout<<"SLP Calc de: "<<num.weight<<endl;
+			_rFloat s=0;
+			_rInt i;
+			for(i=0; i<num.weight; i++)s+=data[i]*weight[i];
+			return s+=weight[i];
 		}
 		public:void train(_rFloat*data,_rFloat rate, _rFloat(*dcostFunction)(_rFloat,_rFloat)/*output-y, target-y*/){
 			_rFloat s=0;
@@ -81,7 +104,7 @@ namespace SLPa{
 			for(i=0; i<num.weight; i++)s+=data[i]*weight[i];
 			s+=weight[i];
 
-			s=dcostFunction(funcs.activatoin(s),data[num.weight])*funcs.dactivatoin(s);
+			s=dcostFunction(funcs.activation(s),data[num.weight])*funcs.dactivation(s);
 			for(i=0; i<num.weight; i++)tmp[i]=weight[i]-rate*s*data[i];
 			tmp[i]=weight[i]-rate*s;
 
@@ -98,14 +121,19 @@ namespace SLPa{
 }
 
 namespace MLPa{
+	using namespace std;
 	template<class _rInt, class _rFloat>
 	class MLPa{
 		private:struct{
 			_rInt layer;
+			_rInt maxSlp;
 		}num;
 
 		private:typedef struct _sS1{
 			SLPa::SLPa<_rInt,_rFloat> slp;
+			struct{
+				_rFloat c, a, f; //cost, activationFunc, sumFunc
+			} cache;
 			struct{		//functions for calculatin & training
 				_rFloat (*Activation)(_rFloat);
 			}funcs;
@@ -117,24 +145,104 @@ namespace MLPa{
 		}_sS0;
 		private:_sS0*layer;
 
-		_rFloat (*dCost)(_rFloat,_rFloat);//output-y, target-y
-
 		public:MLPa(_rInt numOfLayers, _rInt*numOfPerceptrons, _rInt*numOfInputs){
+			this->num.maxSlp=-1;
 			//Init 1
 			this->num.layer=numOfLayers;
 			this->layer=new _sS0[numOfLayers];
 			//Init 2
 			_rInt i=0,j;
 			this->layer[i].num.slp=numOfPerceptrons[i];
+			if(this->layer[i].num.slp > this->num.maxSlp)this->num.maxSlp=this->layer[i].num.slp;
 			this->layer[i].slp=new _sS1[this->layer[i].num.slp];
-			for(j=0;j<this->layer[i].num.slp;j++)this->layer[i].slp[j].slp=SLPa::SLPa<_rInt, _rFloat>(numOfInputs[j]);
+			for(j=0;j<this->layer[i].num.slp;j++){
+				this->layer[i].slp[j].slp=SLPa::SLPa<_rInt, _rFloat>(numOfInputs[j]);
+			}
 			
 			for(i=1;i<numOfLayers;i++){
 				this->layer[i].num.slp=numOfPerceptrons[i];
 				this->layer[i].slp=new _sS1[this->layer[i].num.slp];
-				for(j=0;j<this->layer[i].num.slp;j++)this->layer[i].slp[j].slp=SLPa::SLPa<_rInt, _rFloat>(numOfPerceptrons[i-1]);
+				if(this->layer[i].num.slp > this->num.maxSlp)this->num.maxSlp=this->layer[i].num.slp;
+				for(j=0;j<this->layer[i].num.slp;j++){
+					this->layer[i].slp[j].slp=SLPa::SLPa<_rInt, _rFloat>(numOfPerceptrons[i-1]);
+				}
 			}
+			std::cout<<"maxSlp "<<this->num.maxSlp<<std::endl;
 		}
+
+		//Member functions
+		public:_rFloat*calc(_rFloat*data[], _rFloat*ret){
+			std::cout<<"C0"<<std::endl;
+			_rFloat tmp[2][num.maxSlp];
+			_rInt i=0,j;
+			for(j=0;j<layer[i].num.slp;j++)tmp[i&1][j]=layer[i].slp[j].slp.calc(data[j]);
+			for(i=1;i<num.layer;i++)for(j=0;j<layer[i].num.slp;j++)tmp[i&1][j]=layer[i].slp[j].slp.calc(tmp[i&1^1]);
+			for(j=0;j<layer[num.layer-1].num.slp;j++)ret[j]=tmp[i&1^1][j];
+			std::cout<<layer[num.layer-1].num.slp<<std::endl;
+			return ret;
+		}
+		public:void train(_rFloat*data[], _rFloat rate, _rFloat(*dcostFunction)(_rFloat,_rFloat)/*output-y, target-y*/){
+			_rInt i,j,k;
+			_rFloat tmp[2][num.maxSlp];
+			//Forward Propagation
+			i=0;
+			for(j=0;j<layer[i].num.slp;j++)
+				layer[i].slp[j].cache.a=tmp[i&1][j]=layer[i].slp[j].slp.funcs.activation(layer[i].slp[j].cache.f=layer[i].slp[j].slp.calcDeAct(data[j]));
+			for(i=1;i<num.layer;i++)for(j=0;j<layer[i].num.slp;j++)
+				layer[i].slp[j].cache.a=tmp[i&1][j]=layer[i].slp[j].slp.funcs.activation(layer[i].slp[j].cache.f=layer[i].slp[j].slp.calcDeAct(tmp[i&1^1]));
+
+			//Backward Propagation
+			i=num.layer-1;
+			
+			for(j=0;j<layer[i].num.slp;j++){
+				layer[i].slp[j].cache.c = dcostFunction(layer[i].slp[j].cache.a, data[layer[0].num.slp][j]) * layer[i].slp[j].slp.funcs.dactivation(layer[i].slp[j].cache.f), (cout<<"cache.c : "<<layer[i].slp[j].cache.c<<endl);
+//				std::cout<<"E "<<dcostFunction(layer[i].slp[j].cache.a, data[layer[0].num.slp][j])<<std::endl;
+				for(k=0;k<layer[i].slp[j].slp.num.weight;k++)
+					layer[i].slp[j].slp.weight[k] -= rate * layer[i].slp[j].cache.c * layer[i-1].slp[k].cache.a, (std::cout<<"cache.a : "<<layer[i-1].slp[k].cache.a<<'\t'<<std::endl);
+				layer[i].slp[j].slp.weight[k] -= rate * layer[i].slp[j].cache.c;
+			}
+			for(i--;i>0;i--){
+				for(j=0;j<layer[i].num.slp;j++){
+					for(layer[i].slp[j].cache.c=0,k=0;k<layer[i+1].num.slp;k++)layer[i].slp[j].cache.c+=layer[i+1].slp[k].cache.c * layer[i+1].slp[k].slp.weight[j];
+					layer[i].slp[j].cache.c*=layer[i].slp[j].slp.funcs.dactivation(layer[i].slp[j].cache.f);
+					layer[i].slp[j].cache.c/=layer[i+1].num.slp;
+					for(k=0;k<layer[i].slp[j].slp.num.weight;k++)
+						layer[i].slp[j].slp.weight[k]-=rate * layer[i].slp[j].cache.c * layer[i-1].slp[k].cache.a, (std::cout<<layer[i].slp[j].cache.c * layer[i-1].slp[k].cache.a<<'\t'<<std::endl);
+					layer[i].slp[j].slp.weight[k]-=rate * layer[i].slp[j].cache.c, (std::cout<<layer[i].slp[j].cache.c * layer[i-1].slp[k].cache.a<<'\t'<<std::endl);
+				}
+			}
+			for(j=0;j<layer[i].num.slp;j++){
+				for(layer[i].slp[j].cache.c=0,k=0;k<layer[i+1].num.slp;k++)layer[i].slp[j].cache.c+=layer[i+1].slp[k].cache.c * layer[i+1].slp[k].slp.weight[j];
+				layer[i].slp[j].cache.c*=layer[i].slp[j].slp.funcs.dactivation(layer[i].slp[j].cache.f);
+				layer[i].slp[j].cache.c/=layer[i+1].num.slp;
+				for(k=0;k<layer[i].slp[j].slp.num.weight;k++)
+					layer[i].slp[j].slp.weight[k]-=rate * layer[i].slp[j].cache.c * data[j][k], (std::cout<<layer[i].slp[j].cache.c * data[j][k]<<'\t'<<std::endl);
+				layer[i].slp[j].slp.weight[k]-=rate * layer[i].slp[j].cache.c, (std::cout<<layer[i].slp[j].cache.c * data[j][k]<<'\t'<<std::endl);
+			}
+			std::cout<<"E\n";
+		}
+		public:void setLayerActivationFunc(_rInt layer,_rFloat (*activationFunc)(_rFloat)){
+			std::cout<<"setA : "<<layer<<" "<<(void*)activationFunc<<std::endl;
+			std::cout<<this->layer[layer].num.slp<<std::endl;
+			_rInt i;
+			for(i=0;i<this->layer[layer].num.slp;i++)this->layer[layer].slp[i].slp.funcs.activation=activationFunc;
+		}
+		public:void setLayerDActivationFunc(_rInt layer,_rFloat (*dactivationFunc)(_rFloat)){
+			std::cout<<"setDA : "<<layer<<" "<<(void*)dactivationFunc<<std::endl;
+			std::cout<<this->layer[layer].num.slp<<std::endl;
+			_rInt i;
+			for(i=0;i<this->layer[layer].num.slp;i++)this->layer[layer].slp[i].slp.funcs.dactivation=dactivationFunc;
+		}
+		public:void _p(){
+			_rInt i,j;
+			std::cout<<"MLP Print"<<std::endl;
+			for(i=0;i<num.layer;i++){
+				for(j=0;j<layer[i].num.slp;j++)layer[i].slp[j].slp._p();
+				std::cout<<std::endl;
+			}
+			std::cout<<"MLP Print E"<<std::endl;
+		}
+		public:void _s(_rInt layer, _rInt vertex, _rInt node, _rFloat n){this->layer[layer].slp[vertex].slp._s(node,n);}
 	};
 }
 #endif//__MLP_alpha__
--- a.cpp
+++ a.cpp
@@ -1,7 +1,11 @@
 #include<iostream>
 #include"MLP_alpha/MLP_alpha.h"
+#define _A2P(T) (T*)(T[])
 using namespace std;
 long double dCost(long double o, long double t){return 2*(o-t);}
+long double ELU(long double x){return x<0 ? expl(x)-1 : x;}
+long double dELU(long double x){return x<0 ? expl(x) : 1;}
+long double _tanh(long double x){return (tanh(x)+1)/2;}
 int main(){
 	{
 		cout<<"H"<<endl;
@@ -19,13 +23,64 @@ int main(){
 		a._s(1,1.0);
 		a._p();
 		long double l0[]={2,-2};
-		long double l1[4][3]={{0,0,-1},{0,1,-1},{1,0,-1},{1,1,1}};
-		for(int64_t i=1<<20;i;i--)for(int j=3;j+1;j--)a.train(l1[j],0.01,dCost);
+		long double l1[4][3]={{0,0,-1},{0,1,1},{1,0,1},{1,1,1}};
+		for(int64_t i=1<<10;i;i--)for(int j=3;j+1;j--)a.train(l1[j],0.01,dCost);
 		cout<<a.calc(l1[0])<<' '<<a.calc(l1[1])<<' '<<a.calc(l1[2])<<' '<<a.calc(l1[3])<<endl;
 	}
 	cout<<"H1"<<endl;
+//	{
+//		int l0[]={3,4,5,2}, l1[]={2,3,4,7,3,1,7};
+//		long double tl0={};
+//		long double*l2[]={
+//			_A2P(long double){2.0,4.0,7.7},
+//			_A2P(long double){2.0,4.0,7.0,4.2},
+//			_A2P(long double){-8.0,2.0,-1.0,4.0,3.2},
+//			_A2P(long double){2.0,4.0}};
+//		long double*l3[]={};
+//
+//		MLPa::MLPa<int, long double> a(2, l0, l1);
+//		a.setLayerActivationFunc(0,&linear);
+//		a.setLayerActivationFunc(1,&linear);
+//		a.setLayerActivationFunc(2,&linear);
+//		a.setLayerActivationFunc(3,&linear);
+//		cout<<"linear : "<<(void*)linear<<endl;
+//		long double ret[16];
+//		cout<<l2[0]<<endl;
+//		a.calc(l2,ret);
+//		for(int i=0;i<16;i++)cout<<ret[i]<<' ';cout<<endl;
+//	}
 	{
-		int l0[]={3,4,5,2}, l1[]={2,3,4};
-		MLPa::MLPa<int, long double> a(4, l0, l1);
+		long double tl0[]={0,0}, tl0r[]={0};
+		long double tl1[]={0,1}, tl1r[]={0};
+		long double tl2[]={1,0}, tl2r[]={0};
+		long double tl3[]={1,1}, tl3r[]={0};
+		long double *l2[][3]={
+			{tl0,tl0,tl0r},
+			{tl1,tl1,tl1r},
+			{tl2,tl2,tl2r},
+			{tl3,tl3,tl3r},
+		};
+		int l0[]={2,2,1}, l1[]={2, 2};
+		long double r[16];
+
+		MLPa::MLPa<int, long double> a(3,l0,l1);
+		a.setLayerActivationFunc(0,&ELU);
+		a.setLayerActivationFunc(1,&ELU);
+		a.setLayerActivationFunc(2,&_tanh);
+
+		a.setLayerDActivationFunc(0,&dELU);
+		a.setLayerDActivationFunc(1,&dELU);
+		a.setLayerDActivationFunc(2,&_dtanh);
+
+		a._p();
+
+		for(int t=1<<10;t;t--)for(int i=0;i<4;i++)a.train(l2[i], 0.001, dCost);
+
+		a._p();
+
+		for(int i=0;i<4;i++){
+			a.calc(l2[i], r);
+			for(int j=0;j<16;j++)cout<<r[j]<<' ';cout<<endl;
+		}
 	}
 }
